a7592bacba41a4e5e91b8e1daa0ff9fb77792ba0
kubir <kubirthan7991@example.com>
2024-03-08 14:00:22 +0530

    library management sytsem

---
+++ API/API/Program.cs
@@ -0,0 +1,62 @@
+using API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>(o => {
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
+});
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("myCorspolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:53470").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = "localhost",
+        ValidAudience = "localhost",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]!)),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddScoped<JwtService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("myCorspolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
